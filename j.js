deb7a1d07c4837d14225c4d188928bc42b68f651
aryanjin566-maker <aryanjin566@gmail.com>
2026-02-11 14:53:17 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,7 @@
+
+// input value from user  from console
+var number1 = parseFloat(prompt('Enter the first number:'));
+var number2 = parseFloat(prompt('Enter the second number:'));
+var sum = number1 + number2;
+console.log('The sum is: ' + sum);
+
